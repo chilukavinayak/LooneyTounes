cca3505ade8c94773741dbeadda73ad11baff1e1
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-08-26 10:47:19 +0530

    Diameter of BST Optimal

--- DsAlgo/src/main/java/com/java/learn/dsalgo/trees/generic/DiaMeterOfBST.java
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/trees/generic/DiaMeterOfBST.java
@@ -20,7 +20,15 @@ class TreeNode{
 public class DiaMeterOfBST {
 
 
-
+    public class BSTPair{
+        int dia;
+        int ht;
+        public BSTPair(int dia,int ht){
+            this.dia = dia;
+            this.ht = ht;
+        }
+        public BSTPair(){}
+    }
 
     //height in terms of edges
     public int height(TreeNode node){
@@ -47,5 +55,24 @@ public class DiaMeterOfBST {
 
         return Math.max(Math.max(ld,rd),lh+rh+2);
 
+    }
+
+    public BSTPair diaMeter_Optimal(TreeNode node){
+        if(node == null){
+           BSTPair base = new BSTPair();
+           base.dia = 0;
+           base.ht = -1;
+           return base;
+        }
+
+        BSTPair ld = diaMeter_Optimal(node.left);
+        BSTPair rd = diaMeter_Optimal(node.right);
+
+
+        BSTPair myPair = new BSTPair();
+        myPair.ht = Math.max(ld.ht,rd.ht)+1;
+        myPair.dia = Math.max(Math.max(ld.dia,rd.dia), ld.ht + rd.ht + 2);
+
+        return myPair;
     }
 }
