cacc32d6a9ddd05f8d627216ec39aba9d40cada4
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-08-30 17:14:56 +0530

    Builder Pattern step1
    Car Builder Example1

---
+++ DsAlgo/src/main/java/com/java/learn/designpattern/creational/builder/example1/Car.java
@@ -0,0 +1,10 @@
+package com.java.learn.designpattern.creational.builder.example1;
+
+public class Car {
+    private Indicator indicator;
+    private Transmission transmission;
+    private Engine engine;
+    private GPSNavigation navigation;
+    private int numberOfSeats;
+    private CarType carType;
+}
