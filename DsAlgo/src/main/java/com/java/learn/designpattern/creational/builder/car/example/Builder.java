2dbf84c68790d64df4e0a0f9032e030d1a471481
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-08-30 17:33:03 +0530

    Builder Interface

---
+++ DsAlgo/src/main/java/com/java/learn/designpattern/creational/builder/car/example/Builder.java
@@ -0,0 +1,11 @@
+package com.java.learn.designpattern.creational.builder.car.example;
+
+import com.java.learn.designpattern.creational.builder.car.example.component.*;
+
+public interface Builder {
+    void setIndicator(Indicator indicator);
+    void setEngine(Engine engine);
+    void setCarType(CarType type);
+    void setGPSNavigation(GPSNavigation gpsNavigation);
+    void setTranmission(Transmission tranmission);
+}
