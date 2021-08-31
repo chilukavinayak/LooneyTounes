2fe2661ebc177f7a320fb623b8eb621ac8a11789
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-08-31 19:11:37 +0530

    example8

---
+++ DsAlgo/src/main/java/com/java/learn/java8/example8.java
@@ -0,0 +1,43 @@
+package com.java.learn.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+
+public class example8 {
+    public static void main(String[] args) {
+
+        final List<String> friends =
+                Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+
+        //Method-1
+        String foundName = null;
+        for(String friend: friends){
+            if(friend.startsWith("N")){
+                foundName = friend;
+                break;
+            }
+        }
+
+        if(foundName == null)
+            System.out.println("Not found");
+        else
+            System.out.println(foundName);
+
+
+        //Method-2
+        pickName(friends,"N");
+        pickName(friends,"Z");
+
+    }
+    public static void pickName(List<String> friends, String letter){
+
+        Optional<String> found = friends.stream().filter(s->s.startsWith(letter)).findFirst();
+        System.out.println(String.format("Friend starts with %s and Name -> %s",letter, found.orElse("not found")));
+    }
+
+}
