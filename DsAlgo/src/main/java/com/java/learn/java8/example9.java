0826b762332b3a495ee3e7514d49cae90d39a7c1
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-08-31 19:36:49 +0530

    example9

--- DsAlgo/src/main/java/com/java/learn/java8/example9.java
+++ DsAlgo/src/main/java/com/java/learn/java8/example9.java
@@ -20,10 +20,10 @@ public class example9 {
         //longest one. If there is more than one name with the same longest length,
         //we’ll display the first one we find.
 
-        Optional<String> longestString = friends.stream().reduce((name1,name2)->name1.length()>name2.length()?name1:name2);
+        Optional<String> longestString = friends.stream().reduce((name1,name2)->name1.length()>=name2.length()?name1:name2);
         longestString.ifPresent(System.out::println);
 
-        Optional<Integer> longestLenth = friends.stream().map(s ->s.length()).reduce((a, b)->a>b?a:b);
+        Optional<Integer> longestLenth = friends.stream().map(s ->s.length()).reduce((a, b)->a>=b?a:b);
         longestLenth.ifPresent(System.out::println);
     }
 
