a1e0d97344eeee89c55a45135c44d3a7cd868cb1
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-08 11:59:10 +0530

    Observer Pattern Step-4

--- DsAlgo/src/main/java/com/java/learn/designpattern/behavioural/observer/Subscriber.java
+++ DsAlgo/src/main/java/com/java/learn/designpattern/behavioural/observer/Subscriber.java
@@ -1,13 +1,95 @@
 package com.java.learn.designpattern.behavioural.observer;
 
 
-class Subject{
+import java.util.ArrayList;
+import java.util.List;
 
+class Subject{
+    private int state = 9999; //default state
+    private List<Observer> observers = new ArrayList<>();
+
+
+    public void subscribe(Observer observer){
+        observers.add(observer);
+    }
+
+    public boolean unsubscribe(Observer observer){
+        return observers.remove(observer);
+    }
+
+    private void notifyThem(){
+        for(Observer observer: observers){
+            observer.update();
+        }
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state){
+        this.state = state;
+        notifyThem();
+    }
 }
 
 abstract class Observer{
     //me as (observer) interested in particular subject
     protected Subject subject;
-    //update me any changes in interesed subject
+    //update me any changes in interested subject
     abstract protected void update();
+}
+
+class BinaryObserver extends  Observer{
+
+    public BinaryObserver(Subject subject){
+        this.subject = subject;
+    }
+
+    @Override
+    protected void update() {
+        System.out.println("BinaryObserver:  "+ subject.getState() + " : " + Integer.toBinaryString(subject.getState()) );
+    }
+}
+
+class OctalObserver extends  Observer{
+
+    public OctalObserver(Subject subject){
+        this.subject = subject;
+    }
+
+    @Override
+    protected void update() {
+        System.out.println("OctalObserver:  "+ subject.getState() + " : " + Integer.toOctalString(subject.getState()) );
+    }
+}
+class HexObserver extends  Observer{
+
+    public HexObserver(Subject subject){
+        this.subject = subject;
+    }
+
+    @Override
+    protected void update() {
+        System.out.println("HexObserver:  "+ subject.getState() + " : " + Integer.toHexString(subject.getState()) );
+    }
+}
+class Client{
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        Observer hexObserver = new HexObserver(subject);
+        Observer octObserver = new OctalObserver(subject);
+        Observer binaryObserver = new BinaryObserver(subject);
+
+        subject.subscribe(hexObserver);
+        subject.subscribe(octObserver);
+        subject.subscribe(binaryObserver);
+
+        //first change
+        subject.setState(10);
+
+        //second change
+        subject.setState(15);
+    }
 }
