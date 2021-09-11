b1365df4ef3d130dbde319e4bd762a7a63c28ea7
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-11 20:47:49 +0530

    Program6_Logarithamic

---
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/recusion/Program6_Logarithamic.java
@@ -0,0 +1,26 @@
+package com.java.learn.dsalgo.recusion;
+
+public class Program6_Logarithamic {
+    public static void main(String[] args) {
+        long ans = power(2,10);
+        System.out.println(ans);
+        ans = power(5,13);
+        System.out.println(ans);
+    }
+    public static long power(int x,int n){
+        if(n == 0)
+            return 1;
+
+
+        long xpnb2 =  power(x,n/2);
+
+        long xn =xpnb2*xpnb2;
+
+
+        if(n%2 == 1)
+            xn = xn * x;
+
+
+        return xn;
+    }
+}
