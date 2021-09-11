6988e7f2cb3a575790afe48e6a123afcc7de67d8
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-11 14:32:57 +0530

    Program5 Factorial

---
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/recusion/Program4_Factorial.java
@@ -0,0 +1,13 @@
+package com.java.learn.dsalgo.recusion;
+
+public class Program4_Factorial {
+    public static void main(String[] args) {
+        int ans = factorial(5);
+        System.out.println(ans);
+    }
+    static public int factorial(int n){
+        if(n <= 1)
+            return n;
+        return n * factorial(n-1);
+    }
+}
