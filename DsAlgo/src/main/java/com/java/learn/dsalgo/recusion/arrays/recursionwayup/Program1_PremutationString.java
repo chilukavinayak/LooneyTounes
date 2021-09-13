ebfaaf44055ca7f2b3bd471ddfc47658fe4a5488
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-13 12:23:08 +0530

    Permutation of String

---
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/recusion/arrays/recursionwayup/Program1_PremutationString.java
@@ -0,0 +1,23 @@
+package com.java.learn.dsalgo.recusion.arrays.recursionwayup;
+
+public class Program1_PremutationString {
+    public static void main(String[] args) {
+        String str = "abc";
+        permutation(str,"");
+    }
+    //bc,a |
+    private static void permutation(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){ // 1
+            char ch = str.charAt(i);//a
+            String left = str.substring(0,i);//""
+            String right = str.substring(i+1);//""
+            String ros = left+right; // ""+""
+            permutation(ros,ans+ch); // "",abc
+        }
+    }
+}
