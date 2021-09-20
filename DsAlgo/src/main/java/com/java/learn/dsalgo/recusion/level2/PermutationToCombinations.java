86619e5ccc6c970ed1e957fcad3b9c219ce8c84b
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-20 19:14:23 +0530

    PermutationToCombinations

---
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/recusion/level2/PermutationToCombinations.java
@@ -0,0 +1,37 @@
+package com.java.learn.dsalgo.recusion.level2;
+
+import java.util.Scanner;
+
+public class PermutationToCombinations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tb = sc.nextInt();
+        int ts = sc.nextInt();
+
+        printCombinations(new boolean[tb],tb,ts,-1,0);
+    }
+
+    private static void printCombinations(boolean[] boxes,int tb, int ts, int cb, int cs) {
+        if(cb == tb)
+            return;
+
+        if(cs == ts){
+            for(int i=0;i<boxes.length;i++){
+                if(boxes[i])
+                    System.out.print("i");
+                else
+                    System.out.print("-");
+            }
+            System.out.println();
+        }
+
+
+
+        for(int b = cb+1;b<tb;b++){
+            boxes[b] = true;
+            printCombinations(boxes,tb,ts,b,cs+1);
+            boxes[b] = false;
+
+        }
+    }
+}
