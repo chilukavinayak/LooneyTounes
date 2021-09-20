ea3d1e198b2a6c834778f8bc9a5248c6e8e84f35
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-09-21 00:33:55 +0530

    LengthOfLongestSubStringNoRepeatingCharacters

---
+++ DsAlgo/src/main/java/com/java/learn/dsalgo/slidingwindow/LengthOfLongestSubStringNoRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package com.java.learn.dsalgo.slidingwindow;
+
+import java.util.HashMap;
+
+public class LengthOfLongestSubStringNoRepeatingCharacters {
+    public static void main(String[] args) {
+
+        int ans1 = findLength("aabccbb"); // abc -> o/p -> 3
+        int ans2 = findLength("abbbb"); // abbbb -> ab -> 2
+        System.out.println(ans1);
+        System.out.println(ans2);
+    }
+
+    private static int findLength(String str) {
+
+        HashMap<Character,Integer> freqMap = new HashMap<>();
+        int s=0, maxLength = 0;
+        for(int e=0;e<str.length();e++){
+            Character rc = str.charAt(e);
+            if(freqMap.containsKey(rc)){
+                s = Math.max(s, freqMap.get(rc)+1);
+            }
+            freqMap.put(rc,e);
+            maxLength = Math.max(maxLength,e-s+1);
+
+        }
+        return maxLength;
+    }
+}
