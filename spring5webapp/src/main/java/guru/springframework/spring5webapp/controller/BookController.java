cf2b4f5a1c2356f7d278a9720603914da061e180
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2021-11-06 20:17:14 +0530

    mvc design part done

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.controller;
 
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookController {
 
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
-    public BookController(BookRepository repository) {
+    public BookController(BookRepository repository, AuthorRepository authorRepository) {
         this.bookRepository = repository;
+        this.authorRepository = authorRepository;
     }
 
     @RequestMapping("/books")
     public String getBooks(Model model){
         model.addAttribute("books", bookRepository.findAll());
         return "books/list";
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthorList(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors/author_list";
     }
 }
